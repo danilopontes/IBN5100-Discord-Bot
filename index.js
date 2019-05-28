0d9c28d6af08fb281e4764b4dd8393ece837862c
DANILO_PONTES <daniloggwp@gmail.com>
2019-05-28 18:26:06 -0400

    File refactored:
    - Included command handler
    - Substituted dotenv to config.json
    - Improved on Message listener

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-require('dotenv').config();
 require('./db');
+const fs = require('fs');
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// COMMANDS ENTRY POINT
-const processCommand = require('./commands/processCommands');
+// MAP ALL COMMANDS
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+commandFiles.forEach(file => {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+});
 
 // ON INIT
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('I, Robot', {type: 'WATCHING'});
 });
+
 
 // LISTEN TO MESSAGE
 client.on('message', msg => {
 
-    if(msg.author == client.user || !msg.content.startsWith('!') || msg.channel.type == 'dm') return;
+    if(msg.author.bot || !msg.content.startsWith(prefix) || msg.channel.type == 'dm') return;
 
-    const fullCommand = msg.content.substr(1);
-    const arrayCommand = fullCommand.split(" ");
-    const primaryCommand = arrayCommand[0];
-    const args = arrayCommand.slice(1);
+    const args = msg.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-    processCommand(primaryCommand, args, msg);
+    if(!client.commands.has(commandName)) {
+         return msg.channel.send('This command does not exist, for information about all available commands, use !commands');
+    }
+
+    const command = client.commands.get(commandName);
+
+    if(command.args && !args.length) {
+        let reply = `You didn't provide valid arguments.\n`;
+
+        if(command.usage) {
+
+            reply += 'Follow bellow the proper way to use this command:\n\n'
+
+            Object.keys(command.usage).forEach(e => {
+                reply += `**${e}** - ${command.usage[e]}\n`;
+            });
+        }
+
+        return msg.channel.send(reply);
+    }
+
+    try {
+        command.execute(args, msg);
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // CONNECT BOT
-client.login(process.env.MY_TOKEN);
+client.login(token);
