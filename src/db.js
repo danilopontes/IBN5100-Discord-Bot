dd58fa5a5072f4a0ca94e367002a1418c209d52b
DANILO_PONTES <daniloggwp@gmail.com>
2019-07-09 13:45:51 -0400

    Refactored DB connection exports
    Previously the connection to DB was made just by requring db.js file.
    
    In this new version we export the function that connects to db, and the connection is made
    outsiede the file.

--- src/db.js
+++ src/db.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const { db } = require('./config.json');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(db, {useNewUrlParser: true, useFindAndModify: false}, (err) => {
-  if(err){
-    console.log(err);
-  } else {
-    console.log("DB connected!");
+class Database {
+
+  static connect(){
+    mongoose.Promise = global.Promise;
+    mongoose.connect(db, {useNewUrlParser: true, useFindAndModify: false}, (err) => {
+      if(err){
+        console.log(err);
+      } else {
+        console.log("DB connected!");
+      }
+    });
   }
-});
+}
+
+module.exports = Database;
